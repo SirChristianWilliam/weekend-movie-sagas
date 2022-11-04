879979966cf129da2ac58e4245f3a7854d97218a
Sam Phipps <samuel.v.phipps@gmail.com>
2022-11-03 19:11:21 -0500

    ran testing to determine pool request for genre listing

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,7 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
+
+     const qryText = `
+     SELECT "movies"."id", "movies"."title", "genres"."name" FROM "movies"
+     JOIN "movies_genres"
+	     ON "movies"."id" = "movies_genres"."movie_id"
+     JOIN "genres"
+	     ON "movies_genres"."genre_id" = "genres"."id"
+     WHERE "movies"."id" = 1;
+     `;
+     pool.query(qryText)
+          .then(result => {
+               console.log(result.rows)
+          })
+
   res.sendStatus(500)
 });
 
---
+++ src/components/Details/Details.js
@@ -0,0 +1,18 @@
+function Details(){
+
+     return(
+          <article className="movieDetailContainer">
+               <h1> MOVIE TITLE </h1>
+               <img src="https://s.yimg.com/uu/api/res/1.2/0GBXd289dMqVEPaqTl4x5w--~B/aD0xNTAwO3c9MTAzNTthcHBpZD15dGFjaHlvbg--/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/d05a3f087fa57f6d41b865d53a42a5f5"></img>
+               <p>MOVIE DESCRIPTION</p>
+               <ul>
+                    <li>Space Movie</li>
+                    <li>Action</li>
+               </ul>
+
+          </article>
+     )
+};
+
+
+export default Details;
